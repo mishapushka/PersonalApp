0655cafc3067e5a62ad7a5739b62a3ea67e5a3c1
Mikhail <bskfrost3@yandex.ru>
2020-10-24 19:09:22 +0700

    Initial Commit

---
+++ PersonalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalApp
+//
+//  Created by mac on 24.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
