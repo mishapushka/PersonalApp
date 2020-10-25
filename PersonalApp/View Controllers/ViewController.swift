c4cba1b6c8eb30ee5c761ca68bbd1885c456414a
Mikhail <bskfrost3@yandex.ru>
2020-10-25 13:11:12 +0700

    завелось приложение, осталось написать логику

--- PersonalApp/View Controllers/ViewController.swift
+++ PersonalApp/View Controllers/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var loginInButton: UIButton!
+    
+    @IBOutlet var forgotUserName: UIButton!
+    @IBOutlet var forgotPassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginInAction() {
+    }
+    
+    @IBAction func forgotUserNameAction() {
+    }
+    
+    @IBAction func forgotPasswordAction() {
+    }
+    
 }
 
---
+++ PersonalApp/View Controllers/WelcomeUserViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WelcomeUserViewController.swift
+//  PersonalApp
+//
+//  Created by mac on 25.10.2020.
+//
+
+import UIKit
+
+class WelcomeUserViewController: UIViewController {
+
+    @IBOutlet var logOutButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
